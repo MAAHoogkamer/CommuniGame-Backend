a8735e8c98f2659a585db193f03023604169dace
Michiel A. A. Hoogkamer <michiel.hoogkamer@staffbase.com>
2023-03-01 16:37:58 +0100

    New approach: parent DB class and with extended classes for the collections

--- src/main/kotlin/com/staffbase/communigame/persistence/database.kt
+++ src/main/kotlin/com/staffbase/communigame/persistence/database.kt
@@ -1,18 +1,20 @@
+import com.mongodb.client.model.Filters
+import com.mongodb.client.model.Sorts
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.bson.Document
+import org.litote.kmongo.coroutine.CoroutineCollection
 import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.eq
 import org.litote.kmongo.reactivestreams.KMongo
 
-abstract class Database<T : Any>(collectionName: String) {
+abstract class Database<T : Any>(private val collectionName: String) {
     private val client = KMongo.createClient().coroutine
     private val database = client.getDatabase("communigame")
-    protected val collection = database.getCollection<T>(collectionName)
+    protected val collection: CoroutineCollection<T> = database.getCollection(collectionName)
 
-    suspend fun findById(id: String): T? {
-        return collection.findOneById(id)
-    }
-
-    suspend fun findAll(): List<T> {
-        return collection.find().toList()
-    }
-
-
+    abstract suspend fun createNew(newItem: T): Boolean
+    abstract suspend fun getAll(): List<T>
+    abstract suspend fun getTop(limit: Int): List<T>
+    abstract suspend fun getById(id: String): T?
 }
