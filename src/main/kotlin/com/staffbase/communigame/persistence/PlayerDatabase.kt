02887802c315a33bc8de0fcae63c95dc24da0c7e
Michiel A. A. Hoogkamer <michiel.hoogkamer@staffbase.com>
2023-03-07 14:01:26 +0100

    Added PlayerDatabase

---
+++ src/main/kotlin/com/staffbase/communigame/persistence/PlayerDatabase.kt
@@ -0,0 +1,31 @@
+package com.staffbase.communigame.persistence
+
+import com.staffbase.communigame.models.Player
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
+
+class PlayerDatabase(dbName: String) {
+    private val client = KMongo.createClient().coroutine // create a client connection
+    private val database = client.getDatabase(dbName) // get a reference to the database
+    private val collection = database.getCollection<Player>("players") // get a reference to the players collection
+
+    suspend fun getAllPlayers(): List<Player> {
+        return collection.find().toList()
+    }
+
+    suspend fun getPlayerById(id: String): Player? {
+        return collection.findOneById(id)
+    }
+
+    suspend fun addPlayer(player: Player) {
+        collection.insertOne(player)
+    }
+
+    suspend fun updatePlayer(player: Player) {
+        collection.replaceOneById(player.id, player)
+    }
+
+    suspend fun deletePlayerById(id: String) {
+        collection.deleteOneById(id)
+    }
+}
